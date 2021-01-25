46d1401e4d48b5bf1f5501791f644f081666873f
Joaquin Garcia Desktop <jokogarcia@outlook.com.ar>
2021-01-25 09:28:08 -0300

    implementar La Ciudad de Los Naranjos

--- src/app/components/coleccion-home/coleccion-home.component.ts
+++ src/app/components/coleccion-home/coleccion-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/article';
+import { ArticlesService } from 'src/app/services/articles.service';
 
 @Component({
   selector: 'app-coleccion-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColeccionHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private articlesService:ArticlesService) { }
+  articles:Article[];
 
   ngOnInit(): void {
+    this.getArticles();
+  }
+  getArticles(){
+    this.articlesService.searchByTags("coleccion")
+    .subscribe(
+      articles =>{
+        this.articles=articles;
+      }
+    )
   }
 
 }
