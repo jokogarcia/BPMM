3bd75e6adf24e79dd925f574eed9e721d3cca211
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-12-10 14:12:49 +0100

    chore: upgrade to angular 18

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Article} from '../models/article'
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import { environment } from '../../environments/environment';
 
