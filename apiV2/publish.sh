fb66e7f419446eb2ec8a84d96386dd795ec29b47
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-12-11 15:56:29 +0100

    create publish script for apiV2

--- apiV2/publish.sh
+++ apiV2/publish.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+set -ex
 
 cd ../dataImporter
 rm libros.db
-result=$(npm run dev)
-if [ $? -ne 0 ]; then
-  echo "Data import failed. Aborting deployment."
-  exit 1
-fi
-echo "$result" | grep "Database created successfully. Uploading to server..."
-result=$(rsync -av --progress libros.db joaquin@irazu.com.ar:~/repos/BPMM/apiV2/data/libros.db)
-if [ $? -ne 0 ]; then
-  echo "Data upload failed. Aborting deployment."
-  exit 1
-fi
+npm run dev
 
+ssh joaquin@irazu.com.ar "cd ~/repos/BPMM && git checkout main && git fetch && git pull"
+rsync -av --progress libros.db joaquin@irazu.com.ar:~/repos/BPMM/apiV2/data/libros.db
 
-ssh joaquin@irazu.com.ar "cd ~/repos/BPMM && git checkout main && git fetch && git pull && docker-compose down && docker-compose up -d --build"
+ssh joaquin@irazu.com.ar "cd ~/repos/BPMM/apiV2 && docker compose down && docker compose up -d --build"
