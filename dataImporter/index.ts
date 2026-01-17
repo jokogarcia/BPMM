429b2ee2fe53db4e250c867ff4920cb0c17275e9
Joaquin Garcia <jokogarcia@gmail.com>
2026-01-17 15:05:54 +0100

    adapt dataImporter to new xlx format

--- dataImporter/index.ts
+++ dataImporter/index.ts
@@ -1,14 +1,10 @@
 import { read, WorkSheet } from 'xlsx';
 import * as fs from 'fs';
 import * as sqlite3 from 'sqlite3';
-const novelasFile = fs.readFileSync(`${__dirname}/xls/Novelas 2019.xlsx`);
-const autoresRiojanosFile = fs.readFileSync(`${__dirname}/xls/Cat. Literatura Riojana Exel.xls`);
-const novelas = read(novelasFile);
-const autoresRiojanos = read(autoresRiojanosFile);
+const catalogosFile = fs.readFileSync(`${__dirname}/xls/catalogos.xlsx`);
 
-const autoresRiojanosRows = autoresRiojanos.Sheets[autoresRiojanos.SheetNames[0]];
+const catalogosContent = read(catalogosFile);
 
-const nd1 = novelas.Sheets[novelas.SheetNames[0]];
 function getMaxRow(data: WorkSheet) {
     // Use the worksheet's range property to get the actual range
     if (data['!ref']) {
@@ -38,50 +34,23 @@ function getMaxRow(data: WorkSheet) {
     
     return maxRow;
 }
-function getRowNovela(row: number, data: WorkSheet) {
+function getRow(row: number, data: WorkSheet) {
     if (row < 4) return null;
-    const cant = data[`A${row}`]?.v;
+    const inventario = data[`A${row}`]?.v;
     const autor = data[`B${row}`]?.v;
     const titulo = data[`C${row}`]?.v;
-    const libri = data[`D${row}`]?.v;
-    const editorial = data[`E${row}`]?.v;
-    const ejemplares = data[`F${row}`]?.v;
-    const paginas = data[`G${row}`]?.v;
-    const edicion = data[`H${row}`]?.v;
-    const inventario = data[`I${row}`]?.v;
-    const isbn = data[`J${row}`]?.v;
-    const observaciones = data[`K${row}`]?.v;
+    const editorial = data[`D${row}`]?.v;
+    const ejemplares = data[`E${row}`]?.v;
+    const paginas = data[`F${row}`]?.v;
+    const edicion = data[`G${row}`]?.v;
+    const isbn = data[`H${row}`]?.v;
+    const signatura = data[`I${row}`]?.v;
+    const signatura2 = data[`J${row}`]?.v;
+    
     if (!titulo) return null;
-    return { cant, autor, titulo, libri, editorial, ejemplares, paginas, edicion, inventario, isbn, observaciones };
+    return { ejemplares, autor, titulo, editorial, paginas, edicion, inventario, isbn, signatura, signatura2 };
 }
-function getRowAutorRiojano(row: number, data: WorkSheet) {
-    if (row < 6) return null;
-    const orden = data[`A${row}`]?.v;
-    const autor = data[`B${row}`]?.v;
-    const no = data[`C${row}`]?.v;
-    const titulo = data[`D${row}`]?.v;
-    const editorial = data[`E${row}`]?.v;
-    const ejemplares = data[`F${row}`]?.v;
-    const paginas = data[`G${row}`]?.v;
-    const edicion = data[`H${row}`]?.v;
-    const inventario = data[`I${row}`]?.v;
-    const isbn = data[`J${row}`]?.v;
-    const observaciones = data[`K${row}`]?.v;
-    if (!titulo) return null;
-    return { orden, autor, no, titulo, editorial, ejemplares, paginas, edicion, inventario, isbn, observaciones };
-}
-interface LibroUnificado {
-    autor?: string;
-    titulo: string;
-    editorial?: string;
-    ejemplares?: number;
-    paginas?: number;
-    edicion?: string;
-    inventario?: string;
-    isbn?: string;
-    observaciones?: string;
-    categoria?: string;
-}
+
 function loadDatabase() {
     return new Promise<void>((resolve, reject) => {
         // Create or open database
@@ -98,6 +67,7 @@ function loadDatabase() {
             });
 
             // Create new table based on LibroUnificado interface
+            db.run("BEGIN TRANSACTION")
             db.run(`CREATE TABLE libros (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 autor TEXT,
@@ -120,61 +90,62 @@ function loadDatabase() {
 
             // Prepare insert statement
             const stmt = db.prepare(`INSERT INTO libros 
-                (autor, titulo, editorial, ejemplares, paginas, edicion, inventario, isbn, observaciones, categoria) 
+                (autor, titulo, editorial, ejemplares, paginas, edicion, inventario, isbn, categoria, observaciones) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
 
             let insertedCount = 0;
-
-            // Load data from Novelas sheet
-            const maxRowNovelas = getMaxRow(nd1);
-            for (let row = 6; row < maxRowNovelas; row++) {
-                const libro = getRowNovela(row, nd1);
-                if (libro) {
-                    stmt.run([
-                        libro.autor || null,
-                        libro.titulo,
-                        libro.editorial || null,
-                        libro.ejemplares || null,
-                        libro.paginas || null,
-                        libro.edicion || null,
-                        libro.inventario || null,
-                        libro.isbn || null,
-                        libro.observaciones || null,
-                        'Novelas'
-                    ]);
-                    insertedCount++;
-                }
+            const cleanLibro = (l:any) => {
+                if (!l) return null;
+                l.autor = l.autor ? l.autor : null;
+                l.editorial = l.editorial ? l.editorial.toLowerCase() : null
+                l.ejemplares = l.ejemplares ? l.ejemplares : null;
+                l.paginas = l.paginas ? l.paginas : null;
+                l.edicion = l.edicion ? l.edicion :null;
+                l.inventario = l.inventario ? l.inventario :null;
+                l.isbn = (l.isbn && l.isbn.length > 4)? l.isbn.toLowerCase() : null;
+                l.signatura = l.signatura ? l.signatura.toLowerCase() : null;
+                l.signatura2 = l.signatura2 ? l.signatura2.toLowerCase() : null;
             }
-
-            // Load data from Autores Riojanos sheet
-            const maxRowAutores = getMaxRow(autoresRiojanosRows);
-            for (let row = 4; row < maxRowAutores; row++) {
-                const libro = getRowAutorRiojano(row, autoresRiojanosRows);
-                if (libro) {
-                    stmt.run([
-                        libro.autor || null,
-                        libro.titulo,
-                        libro.editorial || null,
-                        libro.ejemplares || null,
-                        libro.paginas || null,
-                        libro.edicion || null,
-                        libro.inventario || null,
-                        libro.isbn || null,
-                        libro.observaciones || null,
-                        'Autores Riojanos'
-                    ]);
-                    insertedCount++;
+            for (const sheetName of catalogosContent.SheetNames){
+                console.log("Processing sheet",sheetName)
+                const sheet = catalogosContent.Sheets[sheetName];
+                const maxRow = getMaxRow(sheet);
+                for (let row = 3; row < maxRow; row++){
+                    const libro = getRow(row, sheet);
+                    cleanLibro(libro);
+                    if (libro) {
+                        stmt.run([
+                            libro.autor,
+                            libro.titulo,
+                            libro.editorial,
+                            libro.ejemplares,
+                            libro.paginas,
+                            libro.edicion,
+                            libro.inventario,
+                            libro.isbn,
+                            libro.signatura,
+                            libro.signatura2
+                        ]);
+                        insertedCount++;
+                    }
                 }
+                console.log("Inserted",insertedCount);
             }
-
+           
             stmt.finalize((err) => {
                 if (err) {
                     console.error('Error finalizing statement:', err);
                     return reject(err);
                 }
                 console.log(`Database loaded successfully with ${insertedCount} books`);
-
-                // Close database connection
+                db.run("COMMIT", (err) => {
+                    if (err) {
+                        console.error('Commit failed:', err);
+                        return reject(err);
+                    }
+                    
+                    console.log(`Database loaded: ${insertedCount} books`);
+                    // Close database connection
                 db.close((err) => {
                     if (err) {
                         console.error('Error closing database:', err);
@@ -183,11 +154,12 @@ function loadDatabase() {
                     console.log('Database connection closed');
                     resolve();
                 });
+                });
+                
             });
         });
     });
 }
-
 
 
 
